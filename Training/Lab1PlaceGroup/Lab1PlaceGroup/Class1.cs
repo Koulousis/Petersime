a8f0285cd8eabe926082ca7f636b316fd12ab3b5
Aristotelis Koulousis <aris_pc@hotmail.com>
2022-06-07 12:09:05 +0200

    Try to build a Framing Selector

--- Training/Lab1PlaceGroup/Lab1PlaceGroup/Class1.cs
+++ Training/Lab1PlaceGroup/Lab1PlaceGroup/Class1.cs
@@ -19,30 +19,40 @@ namespace Lab1PlaceGroup
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            //Get application and document objects
-            UIApplication uiapp = commandData.Application;
-            Document doc = uiapp.ActiveUIDocument.Document;
+            try
+            {
+                // Select some elements in Revit before invoking this command
+                // Get the handle of current document.
+                UIDocument uidoc = commandData.Application.ActiveUIDocument;
 
-            //Define a reference Object to accept the pick result
-            Reference pickedref = null;
+                // Get the element selection of current document.
+                Selection selection = uidoc.Selection;
+                ICollection<ElementId> selectedIds = uidoc.Selection.GetElementIds();
 
-            //Pick a group
-            Selection sel = uiapp.ActiveUIDocument.Selection;            
-            pickedref = sel.PickObject(ObjectType.Element, "Please select a group");
-            Element elem = doc.GetElement(pickedref);
-            Group group = elem as Group;
+                if (0 == selectedIds.Count)
+                {
+                    // If no elements selected.
+                    TaskDialog.Show("Revit", "You haven't selected any elements.");
+                }
+                else
+                {
+                    String info = "Ids of selected elements in the document are: ";
+                    foreach (ElementId id in selectedIds)
+                    {
+                        info += "\n\t" + id.IntegerValue;
+                    }
 
-            //Pick point
-            XYZ point = sel.PickPoint("Please pick a point to place group");
+                    TaskDialog.Show("Revit", info);
+                }
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+                return Autodesk.Revit.UI.Result.Failed;
+            }
 
-            //Place the group
-            Transaction trans = new Transaction(doc);
-            trans.Start("Lab");
-            doc.Create.PlaceGroup(point, group.GroupType);
-            trans.Commit();
+            return Autodesk.Revit.UI.Result.Succeeded;
 
-            MessageBox.Show(Result.Succeeded.ToString());            
-            return Result.Succeeded;
         }
 
 
