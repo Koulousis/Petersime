cf194f9a82989bebe12018e1b419cb1c2e2252b9
Aristotelis Koulousis <aris_pc@hotmail.com>
2022-06-09 08:50:05 +0200

    Framing Selector - V1 Done
    By adding OST_StructularFraming to a Filtered Collector, i use Selection.SetElemenIds to select into Revit all the Structular Framing elements.

--- FramingSelector/FramingSelector/App.cs
+++ FramingSelector/FramingSelector/App.cs
@@ -5,6 +5,7 @@ using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using System;
 using System.Collections.Generic;
+using System.Windows.Media.Imaging;
 
 #endregion
 
@@ -14,6 +15,15 @@ namespace FramingSelector
     {
         public Result OnStartup(UIControlledApplication a)
         {
+            RibbonPanel ribbonPanel = a.CreateRibbonPanel("Tools");
+            string ribbonAssembly = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string ribbonAssemblyPath = System.IO.Path.GetDirectoryName(ribbonAssembly);
+
+            PushButtonData pushButtonData = new PushButtonData("Structure Framing", "Selector", ribbonAssembly, "FramingSelector.Command");
+            pushButtonData.LargeImage = new BitmapImage(new Uri(System.IO.Path.Combine(ribbonAssemblyPath, "Tools.png")));
+            PushButton pushButton = (PushButton)ribbonPanel.AddItem(pushButtonData);
+
+
             return Result.Succeeded;
         }
 
