15af32da598b274873a5569c8a013f5016ecc514
Aristotelis Koulousis <aris_pc@hotmail.com>
2022-07-13 15:59:33 +0200

    Tools -> Structure Picker
    Finalized - Version 1

--- Tools/Structure/Picker/App.cs
+++ Tools/Structure/Picker/App.cs
@@ -5,6 +5,8 @@ using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
+using System.Windows.Media.Imaging;
 
 #endregion
 
@@ -17,10 +19,16 @@ namespace Picker
 		//ModelessForm instance
 		private MainForm _mainForm;
 
-		public Result OnStartup(UIControlledApplication a)
+		public Result OnStartup(UIControlledApplication uiapp)
 		{
 			_mainForm = null; //no dialog needed yet; the command will bring it
 			thisApp = this; //static access to this application instance
+
+			RibbonPanel panel = uiapp.CreateRibbonPanel("Petersime V2");
+			//AddPushButton(panel);
+			AddSplitButton(panel);
+			//AddStackedButtons(panel);
+			//AddSlideOut(panel);
 			return Result.Succeeded;
 		}
 
@@ -49,6 +57,30 @@ namespace Picker
 				_mainForm = new MainForm(externalEvent, handler, uiapp);
 				_mainForm.Show();
 			}
+		}
+
+		private void AddPushButton(RibbonPanel panel)
+		{
+			string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+			PushButton pushButton = panel.AddItem(new PushButtonData("Tools", "Structure Tools", thisAssemblyPath, "Picker.Command")) as PushButton;
+
+			// Set ToolTip and contextual help
+			pushButton.ToolTip = "Provides a list of useful tools for the user.";
+			// Set the large image shown on button
+			pushButton.LargeImage =	new BitmapImage(new Uri(@"J:\Drawings REVIT FAMILIES\02 NEW\Aris\Logos\Tools\Tools32x32.png"));
+		}
+
+		private void AddSplitButton(RibbonPanel panel)
+		{
+			string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+
+			// create push buttons for split button drop down
+			PushButtonData bOne = new PushButtonData("Tools", "Structure Picker", thisAssemblyPath, "Picker.Command");
+			bOne.LargeImage = new BitmapImage(new Uri(@"J:\Drawings REVIT FAMILIES\02 NEW\Aris\Logos\Tools\Picker35x35.png"));
+			
+			SplitButtonData sb1 = new SplitButtonData("Tools", "Tools");
+			SplitButton sb = panel.AddItem(sb1) as SplitButton;
+			sb.AddPushButton(bOne);
 		}
 	}
 }
